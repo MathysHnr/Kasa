bd2e9df7abec4bb969c7d804d279fb17bd968592
MathysHnr <henneron.mathyss@gmail.com>
2026-06-29 13:07:53 +0200

    Met en place React Router dans un composant dédié

---
+++ src/router/Router.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from '../components/Layout/Layout'
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop'
+import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
+import Housing from '../pages/Housing/Housing'
+import Error from '../pages/Error/Error'
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/a-propos" element={<About />} />
+                    <Route path="/logement/:id" element={<Housing />} />
+                    <Route path="*" element={<Error />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
